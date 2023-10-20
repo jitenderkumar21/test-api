db5bd86cac1bc794c4238bfd992f35411c447212
Jitender Kumar <70776554+jitenderkumar21@users.noreply.github.com>
2023-10-20 13:45:04 +0530

    new project

---
+++ src/main/java/com/example/coral/CoralApplication.java
@@ -0,0 +1,13 @@
+package com.example.coral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoralApplication.class, args);
+	}
+
+}
